827d1297218db75a4dd360304227ccb8ab7fcb1e
Darién Julián Ramírez <ianshalaga@gmail.com>
2022-11-22 23:09:20 -0300

    splitter added

---
+++ splitter.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from moviepy.editor import VideoFileClip
+import subprocess
+from termcolor import colored
+import os
+
+
+def final_scenes_splitter(mkvmerge_path, videos_path, ending_cut_time):
+  videos = Path(videos_path)
+  for i, video_path in enumerate(videos.iterdir()):
+    print(colored(video_path, "green"))
+    video = VideoFileClip(str(video_path))
+    video_duration = video.duration
+    mkvmerge_list = [
+      mkvmerge_path, # Software
+      f"--split", # Split option
+      str(video_duration - ending_cut_time) + "s", # Split timestamp in seconds
+      video_path, # Video to split
+      "-o", # Output option
+      os.path.join(videos, "%02d.mkv" % (i+1)) # Output path
+    ]
+    subprocess.run(mkvmerge_list)
+
+
+mkvmerge_path = "C:/Program Files/MKVToolNix/mkvmerge.exe"
+videos_path = "E:/Grabaciones/Soulcalibur VI/Peleas/SSLT 3 PC"
+ending_cut_time = 21 # Seconds
+
+final_scenes_splitter(mkvmerge_path, videos_path, ending_cut_time)
--- youtube_format.py
+++ youtube_format.py
@@ -271,8 +271,8 @@ def generar_descripcion(nombre_salida_marcas_tiempo,
             #     if isinstance(db_dict[e], list): # If is a list
             #         player_string += f"{}"
 
-    player_string += "Seyfer Studios Lightning League 1 (PS4): " + "https://www.youtube.com/playlist?list=PL90QAKwVH1t4Z4kFd9QAqGdHprsVtaN9c" + "\n"
-    player_string += "Brackets: " + "https://challonge.com/es/u5nndbg4" + "\n"
+    player_string += "Seyfer Studios Lightning Tournament 19 (PC): " + "https://www.youtube.com/playlist?list=PL90QAKwVH1t6ANnxY6KJQJu6oPN7Htc2e" + "\n"
+    player_string += "Brackets: " + "https://challonge.com/es/3eydh4mr" + "\n"
 
     player_string += "SSLT Rankings: " + "https://ianshalaga.github.io/sc_ranking" + "\n\n"
     player_string += "Seyfer Studios Soul Calibur Events: " + "https://s.team/chat/JeBLJtyq" + "\n\n"
